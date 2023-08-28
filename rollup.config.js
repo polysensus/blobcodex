b2c6ced328127b7ae28dd0ad19bbba6ee6c9f540
Robin Bryce <robin.bryce@jitsuin.com>
2023-08-28 15:09:29 +0100

    build: don't polyfill crypto, it just doesn't work

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 // derived from: https://github.com/rollup/rollup-starter-lib/blob/master/rollup.config.js
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import nodePolyfills from "rollup-plugin-node-polyfills";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json" assert { type: "json" };
 
@@ -10,6 +10,7 @@ export default [
     // where possible
     // name: pkg.name,
     input: "src/main.js",
+    external: ["crypto"],
     output: [
       {
         // inlineDynamicImports: true,
@@ -25,8 +26,7 @@ export default [
       // nodePolyfils so import fs works in node and in browser, on node it is stubbed to the global
       // This needs preferBuiltins to be false in the nodeResolve plugin
       nodePolyfills(),
-      // Note: puting main prioriizes ethers lib/index.js over its lib.esm entry point. and this seems to avoid the bridging issues with js-sha3 and so on
-      nodeResolve({ preferBuiltins: false, mainFields: ["main", "module"] }),
+      nodeResolve({ preferBuiltins: true }),
       commonjs(),
     ],
     // plugins: [json(), resolve(), nodePolyfills(), commonjs()],
